06f9bb33bac65cf01486bdf3eaf10ae0452b4ce7
azuma-ya <kazu.bluemountain@gmail.com>
2023-03-27 09:08:00 +0900

    register

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+
+export default function Login() {
+	return (
+		<div>
+			Login
+			<AccessAlarmIcon />
+		</div>
+	);
+}
